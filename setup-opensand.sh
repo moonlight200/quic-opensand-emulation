1ac9c1820575b3a6663fe112cfe53180067f3152
Felix Beil <felix.beil@tum.de>
2021-01-20 14:50:29 +0100

    opensand: Fix configuration mounting
    Mounting the configuration to /etc/opensand only stayes within the
    current process, thus when mounting inside the ip netns exec
    command, it will be very short lived. By using a new shell within
    the name space, the process stayes as long as the entity runs.

--- setup-opensand.sh
+++ setup-opensand.sh
@@ -2,23 +2,39 @@
 
 OPENSAND_CONFIGS="/home/beil/opensand/config"
 
+declare -F log > /dev/null || function log() {
+	local level="$1"
+	local msg="$2"
+
+	echo "[$level] $msg"
+}
+
 # Start satelite
 log D "Launching satellite into (name-)space"
 sudo ip netns exec ns3 killall opensand-sat -q
-tmux new-session -s opensand-sat -d
-tmux send-keys -t opensand-sat "sudo ip netns exec ns3 mount -o bind ${OPENSAND_CONFIGS}/sat /etc/opensand" Enter
-tmux send-keys -t opensand-sat "sudo ip netns exec ns3 opensand-sat -a 10.3.3.254 -c /etc/opensand" Enter
+tmux new-session -s opensand-sat -d "sudo ip netns exec ns3 bash"
+sleep 0.2
+tmux send-keys -t opensand-sat "mount -o bind ${OPENSAND_CONFIGS}/sat /etc/opensand" Enter
+sleep 0.2
+tmux send-keys -t opensand-sat "opensand-sat -a 10.3.3.254 -c /etc/opensand" Enter
+sleep 0.2
 
 # Start gateway
 log D "Aligning the gateway's satellite dish"
 sudo ip netns exec ns3 killall opensand-gw -q
-tmux new-session -s opensand-gw -d
-tmux send-keys -t opensand-gw "sudo ip netns exec ns4 mount -o bind ${OPENSAND_CONFIGS}/gw /etc/opensand" Enter
-tmux send-keys -t opensand-gw "sudo ip netns exec ns4 opensand-gw -i 0 -a 10.3.3.1 -t tapgw -c /etc/opensand" Enter
+tmux new-session -s opensand-gw -d "sudo ip netns exec ns4 bash"
+sleep 0.2
+tmux send-keys -t opensand-gw "mount -o bind ${OPENSAND_CONFIGS}/gw /etc/opensand" Enter
+sleep 0.2
+tmux send-keys -t opensand-gw "opensand-gw -i 0 -a 10.3.3.1 -t tapgw -c /etc/opensand" Enter
+sleep 0.2
 
 # Start statellite terminal
 log D "Connecting the satellite terminal"
 sudo ip netns exec ns1 killall opensand-st -q
-tmux new-session -s opensand-st -d
-tmux send-keys -t opensand-st "sudo ip netns exec ns1 mount -o bind ${OPENSAND_CONFIGS}/st /etc/opensand" Enter
-tmux send-keys -t opensand-st "sudo ip netns exec ns1 opensand-st -i 1 -a 10.3.3.2 -t tapst -c /etc/opensand" Enter
+tmux new-session -s opensand-st -d "sudo ip netns exec ns1 bash"
+sleep 0.2
+tmux send-keys -t opensand-st "mount -o bind ${OPENSAND_CONFIGS}/st /etc/opensand" Enter
+sleep 0.2
+tmux send-keys -t opensand-st "opensand-st -i 1 -a 10.3.3.2 -t tapst -c /etc/opensand" Enter
+sleep 0.2
--- teardown-opensand.sh
+++ teardown-opensand.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
+declare -F log > /dev/null || function log() {
+	local level="$1"
+	local msg="$2"
+
+	echo "[$level] $msg"
+}
+
 log D "Disconnecting satellite terminal"
 tmux send-keys -t opensand-st C-c
+sleep 0.1
+tmux send-keys -t opensand-st C-d
+sleep 0.1
 sudo ip netns exec ns1 killall opensand-st -q
+sleep 0.1
 tmux kill-session -t opensand-st > /dev/null 2>&1
 
 log D "Shutting down gateway"
 tmux send-keys -t opensand-gw C-c
+sleep 0.1
+tmux send-keys -t opensand-gw C-d
+sleep 0.1
 sudo ip netns exec ns1 killall opensand-gw -q
+sleep 0.1
 tmux kill-session -t opensand-gw > /dev/null 2>&1
 
 log D "Desintegrating satellite"
 tmux send-keys -t opensand-sat C-c
+sleep 0.1
+tmux send-keys -t opensand-sat C-d
+sleep 0.1
 sudo ip netns exec ns1 killall opensand-sat -q
+sleep 0.1
 tmux kill-session -t opensand-sat > /dev/null 2>&1
