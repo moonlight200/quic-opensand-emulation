bb5f8b7a94009f77bdacfbb9c94c103fea25f08a
Felix Beil <felix.beil@tum.de>
2021-01-22 17:58:28 +0100

    opensand: Add exit traps and preconditions
    The exit traps are used to abort immediately (instead of only killing
    the current command) and perform a clean exit removing any process
    the script has started.
    
    The preconditions prevent the script from being executed if there are
    indications that another emulation is already running.

--- opensand.sh
+++ opensand.sh
@@ -73,17 +73,59 @@ function log() {
 	fi
 }
 
+# _osnd_cleanup()
+function _osnd_cleanup() {
+    # Ensure all tmux sessions are closed
+	tmux -L ${TMUX_SOCKET} kill-server &> /dev/null
+}
+
+# _osnd_abort_measurements()
+# Trap function executed on the EXIT trap during active measurements.
+function _osnd_abort_measurements() {
+	log E "Aborting measurements"
+	osnd_teardown 2> /dev/null
+	_osnd_cleanup
+}
+
+# _osnd_interrupt_measurement()
+# Trap function executed when the SIGINT signal is received
+function _osnd_interrupt_measurement() {
+	# Don't just stop the current command, exit the entire script instead
+	exit 1
+}
+
+# _osnd_check_running_emulation()
+function _osnd_check_running_emulation() {
+    # Check for running tmux sessions
+
+    if [ ! tmux -L ${TMUX_SOCKET} list-sessions &> /dev/null ]; then
+    	>&2 echo "Active tmux sessions found!"
+    	>&2 echo "Another emulation might already be running, or this is a leftover of a previous run."
+    	>&2 echo "Execute the ./teardown.sh script to get rid of any leftovers."
+    	exit 2
+    fi
+
+    # Check if namespaces exist
+    for ns in $( sudo ip netns list ); do
+    	if [[ "$ns" == "osnd"* ]]; then
+			>&2 echo "Existing namespace $ns!"
+			>&2 echo "Another emulation might already be running, or this is a leftover of a previous run."
+			>&2 echo "Execute the ./teardown.sh script to get rid of any leftovers."
+			exit 3
+    	fi
+    done
+}
+
+# _osnd_create_emulation_dir()
 function _osnd_create_emulation_dir() {
 	if [ -e "$EMULATION_DIR" ]; then
 		>&2 echo "Output directory $EMULATION_DIR already exists"
-		>&2 echo "aborting emulation"
 		exit 4
 	fi
 
 	mkdir -p "$EMULATION_DIR"
 	if [ $? -ne 0 ]; then
 		>&2 echo "Failed to create output directory $EMULATION_DIR"
-		>&2 echo "aborting emulation"
 		exit 5
 	fi
 
@@ -97,22 +139,36 @@ function _osnd_create_emulation_dir() {
 	fi
 }
 
-function main() {
+# _osnd_run_measurements()
+function _osnd_run_measurements() {
+	osnd_run_ping "${EMULATION_DIR}"
+	osnd_run_quic_goodput "${EMULATION_DIR}" false 1
+	osnd_run_quic_timing "${EMULATION_DIR}" false 2
+	osnd_run_tcp_goodput "${EMULATION_DIR}" false 1
+	osnd_run_tcp_timing "${EMULATION_DIR}" false 2
+}
+
+function _main() {
 	# TODO arg parse
+
+	_osnd_check_running_emulation
+
 	emulation_start="$( date +"%Y-%m-%d-%H-%M" )"
 	export EMULATION_DIR="${RESULTS_DIR}/${emulation_start}_opensand"
 	_osnd_create_emulation_dir
 
 	log I "Starting Opensand satellite emulation measurements"
+	trap _osnd_abort_measurements EXIT
+	trap _osnd_int SIGINT
+	_osnd_run_measurements 2> >(log E -)
+	trap - SIGINT
+	trap - EXIT
 
-	osnd_run_ping "${EMULATION_DIR}" 2> >(log E -)
-	osnd_run_quic_goodput "${EMULATION_DIR}" false 1 2> >(log E -)
-	osnd_run_quic_timing "${EMULATION_DIR}" false 2 2> >(log E -)
-	osnd_run_tcp_goodput "${EMULATION_DIR}" false 1 2> >(log E -)
-	osnd_run_tcp_timing "${EMULATION_DIR}" false 2 2> >(log E -)
-
-	# TODO cleanup: kill tmux server (also on trap)
+	_osnd_cleanup
 	log I "Done with all measurements"
 }
 
-main "$@"
+# If script is executed directly
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+	_main "$@"
+fi
